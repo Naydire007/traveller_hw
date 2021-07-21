f8948e01ce6cd9bae80a4362b6ffac0edf59d9a8
Stephanie <stephania.l.e@gmail.com>
2021-07-21 23:04:03 +0100

    10 passing 1 to go

---
+++ models/traveller.js
@@ -0,0 +1,40 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport);
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance >= minDistance);
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => total += journey.distance ,0);
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+ return this.journeys.reduce((ModesOfTransport,journey)=> ModesOfTransport += journey.transport,0);
+
+  reduce 
+  push 
+  include
+
+};
+
+
+module.exports = Traveller;
